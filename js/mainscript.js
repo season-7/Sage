2bc68455c8cc47178fc697d5b9d7664d93083ea5
Brian Lusina <lusinabrian@gmail.com>
2016-07-05 13:13:11 +0300

    Add footer

--- js/mainscript.js
+++ js/mainscript.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
-	//TODO: insert functions
-
+	/*obtain the value of the email*/
+    var email = $('#email-input').val;
+    /*check if the email is valid*/
+    var regex = //i;
+    
 });

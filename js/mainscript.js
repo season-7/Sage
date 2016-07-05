ba8b131842749cf4e5b72db08964df92eb229769
Brian Lusina <lusinabrian@gmail.com>
2016-07-05 16:34:24 +0300

    Add journal section

--- js/mainscript.js
+++ js/mainscript.js
@@ -6,5 +6,23 @@ $(document).ready(function(){
     
     regex.test(email);
         
-    
+
+    //automatic slides  
+    var slideIndex = 0;
+    showSlides();
+
+    function showSlides() {
+        var i;
+        var slides = $('#quote-container').get();
+        
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none"; 
+        }
+        
+        slideIndex++;
+        if (slideIndex> slides.length) {slideIndex = 1} 
+        slides[slideIndex-1].style.display = "block"; 
+        setTimeout(showSlides, 3000); // Change image every 3 seconds  
+    }
+        
 });

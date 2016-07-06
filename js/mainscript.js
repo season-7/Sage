1ce86a028a11dfc710f61fff408d6bc558c460b2
Brian Lusina <lusinabrian@gmail.com>
2016-07-06 11:25:33 +0300

    Add animation

--- js/mainscript.js
+++ js/mainscript.js
@@ -1,10 +1,19 @@
 $(document).ready(function(){
-	/*obtain the value of the email*/
-    var email = $('#email-input').val;
-    /*check if the email is valid*/
+	/*Create a regex expression for the email*/
     var regex = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
     
-    regex.test(email);
+	$('#sign-up-btn').on('click', function(){
+		var emailInput = $('#email-input');
+		/*obtain the value of the email*/
+		var email = $('#email-input').val;		
+		/*Test the validity of the email*/
+		regex.test(email) ? submit(email) : emailInput.addClass('form-error');
+	});
+	
+	/*function to submit the valid email*/
+	function submit(validEmail){
+		
+	}
         
 
     //automatic slides  
@@ -24,10 +33,5 @@ $(document).ready(function(){
         slides[slideIndex-1].style.display = "block"; 
         setTimeout(showSlides, 3000); // Change image every 3 seconds  
     }
-    
-    /*animations for social media icons*/
-    $('.social-icons').on('mouseenter', '.icon-holder', function(){
-        $(this).find('#pinterest-icon').css('color:blue');
-    });
     
 });
